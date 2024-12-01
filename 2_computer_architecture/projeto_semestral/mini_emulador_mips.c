95ae5be10de1aeee8dc24befbd099791e4893db4
Henrique Oliveira <mhenriqueof@outlook.com>
2024-12-01 10:15:32 -0300

    .

--- 2_computer_architecture/projeto_semestral/mini_emulador_mips.c
+++ 2_computer_architecture/projeto_semestral/mini_emulador_mips.c
@@ -15,7 +15,7 @@ void menu_inicial() {
     printf("--- Salto: j, jr, jal\n");
     printf("-- Regras: \n");
     printf("--- Utilizar apenas letras minusculas\n");
-    printf("--- Utilizar somente numeros inteiros e com ate 3 digitos\n");
+    printf("--- Utilizar apenas numeros inteiros e com ate 3 digitos\n");
     printf("--- Utilizar somente 3 registradores do tipo saved [$s0, $s1, $s2]\n");
     printf("--- Nao utilizar um registrador mais de uma vez em uma instrucao\n");
     printf("--- O simulador e limitado a ter apenas 20 instrucoes\n");
